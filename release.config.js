fa5a7c69b30ace2662c5f4430f7982e9ddb47a0d
secondman <secondman@jetstreamlabs.com>
2025-07-11 00:48:01 -0700

    chore(release-config): fix immutable error
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- release.config.js
+++ release.config.js
@@ -37,10 +37,13 @@ const releaseNotesGeneratorOptions = {
         test: 'Code Testing',
       }
 
-      commit.type = types[commit.type]
+      // Create a new commit object instead of modifying the immutable one
+      const transformedCommit = { ...commit }
+
+      transformedCommit.type = types[commit.type] || commit.type
 
       if (typeof commit.hash === 'string') {
-        commit.shortHash = commit.hash.substring(0, 7)
+        transformedCommit.shortHash = commit.hash.substring(0, 7)
       }
 
       if (typeof commit.subject === 'string') {
@@ -48,25 +51,28 @@ const releaseNotesGeneratorOptions = {
         if (url) {
           url = `${url}/issues/`
           // Issue URLs.
-          commit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
+          transformedCommit.subject = commit.subject.replace(/#([0-9]+)/g, (_, issue) => {
             issues.push(issue)
             return `[#${issue}](${url}${issue})`
           })
         }
         if (context.host) {
           // User URLs.
-          commit.subject = commit.subject.replace(/\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g, (_, username) => {
-            if (username.includes('/')) {
-              return `@${username}`
-            }
+          transformedCommit.subject = transformedCommit.subject.replace(
+            /\B@([a-z0-9](?:-?[a-z0-9/]){0,38})/g,
+            (_, username) => {
+              if (username.includes('/')) {
+                return `@${username}`
+              }
 
-            return `[@${username}](${context.host}/${username})`
-          })
+              return `[@${username}](${context.host}/${username})`
+            }
+          )
         }
       }
 
       // remove references that already appear in the subject
-      commit.references = commit.references.filter((reference) => {
+      transformedCommit.references = commit.references.filter((reference) => {
         if (issues.indexOf(reference.issue) === -1) {
           return true
         }
@@ -74,7 +80,7 @@ const releaseNotesGeneratorOptions = {
         return false
       })
 
-      return commit
+      return transformedCommit
     },
   },
 }
