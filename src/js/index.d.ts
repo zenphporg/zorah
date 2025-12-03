fc86a6b6ef8646392a25ebef08d2aaab9d822f30
secondman <secondman@jetstreamlabs.com>
2025-12-03 16:44:45 -0700

    chore: style fixes
    Signed-off-by: secondman <secondman@jetstreamlabs.com>

--- src/js/index.d.ts
+++ src/js/index.d.ts
@@ -33,7 +33,7 @@ declare module 'vue' {
     trans: (
       key: string,
       replace?: { [key: string]: string | number },
-      config?: import('zorah-js').ZorahConfig
+      config?: import('zorah-js').ZorahConfig,
     ) => string;
   }
 }
